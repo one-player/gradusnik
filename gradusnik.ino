574b6c79c8cb2c3c945924232c92da7f84b231bc
dudeden <it@anabel-arto.com>
2018-05-10 17:34:18 +0300

    send_to_server

--- gradusnik.ino
+++ gradusnik.ino
@@ -18,6 +18,7 @@ SCK (Serial Clock)  ->  A5/GPIO-2 on Uno/Pro-Mini, 21 on Mega2560/Due, 3 Leonard
 /*WiFi prop.*/
 const char* ssid = "123";
 const char* password = "k7";
+const char* host = "127.0.0.1";//IP принимающего сервера
 const int httpPort = 80;
 /*WiFi prop.*/
 
@@ -25,9 +26,9 @@ float temp_tmp;
 float hum_tmp;
 float pres_tmp;
 
-//#define SERIAL_BAUD 9600
+unsigned long time;
+
 BME280I2C bme;
-WiFiServer server(80);
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
 WiFiClient client;
@@ -59,7 +60,6 @@ void setup() {
     Serial.print(".");
   }
   Serial.println(" connected");
-  server.begin();
   Serial.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());
   MDNS.begin("Gradusnik");
   httpUpdater.setup(&httpServer);
@@ -69,79 +69,16 @@ void setup() {
 
 void loop() {
   httpServer.handleClient();
-  WiFiClient client = server.available();                // Получаем данные, посылаемые клиентом 
-  if (client){
-    boolean blank_line = true;                            // Создаем переменную, чтобы определить конец HTTP-запроса 
-    while (client.connected()){                           // Пока есть соединение с клиентом 
-      if (client.available()){                            // Если клиент активен 
-        char c = client.read();                            // Считываем посылаемую информацию в переменную "с"
-        if (c == '\n' && blank_line){                      // Вывод HTML страницы 
-          
-          float temp(NAN), hum(NAN), pres(NAN);
-          BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
-          BME280::PresUnit presUnit(BME280::PresUnit_Pa);
-          bme.read(pres, temp, hum, tempUnit, presUnit);
-          
-          client.println("HTTP/1.1 200 OK");               // Стандартный заголовок HTTP 
-          client.println("Content-Type: text/html; charset=utf-8"); 
-          client.println("Connection: close");             // Соединение будет закрыто после завершения ответа
-          client.println("Refresh: 10");                   // Автоматическое обновление каждые 10 сек 
-          client.println();
-          client.println("<!DOCTYPE HTML>");               // Веб-страница создается с использованием HTML
-          client.println("<html>");                        // Открытие тега HTML 
-          client.println("<head><style>body {background-color:#87CEEB; color:#ffffff; text-shadow: 0px 2px 6px rgba(0,0,0,0.4); font-family: Sans-Serif, Arial, Helvetica;}</style>");
-          client.print("<title>Градусник</title>");     // Название страницы
-          client.println("</head>");
-          client.println("<body><center>");
-          
-          client.println("<div><h1>WiFi градусник</h1></div>"); 
-          client.println("<h3>Температура = ");
-          
-          client.println(temp);      // Отображение температуры
-          client.print("*C");
-            if (temp_tmp<=temp)
-            {
-              client.println("<font color='green'> &uArr;</font></h3>");
-              temp_tmp=temp;
-            }else{
-              client.println("<font color='red'> &dArr;</font></h3>");
-              temp_tmp=temp;
-            }          
-          client.println("<h3>Влажность = ");
-          client.println(hum);      // Отображение температуры
-          client.print("% RH");
-            if (hum_tmp<=hum)
-            {
-              client.println("<font color='green'> &uArr;</font></h3>");
-              hum_tmp=hum;
-            }else{
-              client.println("<font color='red'> &dArr;</font></h3>");
-              hum_tmp=hum;
-            } 
-          client.println("<h3>Давление = ");
-          client.println(pres);      // Отображение температуры
-          client.print(" atm");
-          if (pres_tmp<=pres)
-          {
-            client.println("<font color='green'> &uArr;</font></h3>");
-            pres_tmp=pres;
-          }else{
-            client.println("<font color='red'> &dArr;</font></h3>");
-            pres_tmp=pres;
-          }
-          client.println("</center></body>");
-          client.println("</html>");                       // Закрытие тега HTML 
-          break;                                           // Выход
-        }
-          if (c == '\n'){                                 // Если "с" равен символу новой строки                                             
-          blank_line = true;                             // Тогда начинаем новую строку
-        }                                          
-          else if (c != '\r'){                           // Если "с" не равен символу возврата курсора на начало строки                                        
-          blank_line = false;                           // Тогда получаем символ на текущей строке 
-        }                                        
+  WiFiClient client;
+    if (!client.connect(host, httpPort)) 
+      {
+        Serial.println("connection failed");
+        return;
       }
-    }  
+    float temp(NAN), hum(NAN), pres(NAN);
+    BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+    BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+    bme.read(pres, temp, hum, tempUnit, presUnit);
+    client.print(String("GET /insert_param.php?temp=")+temp+"&hum="+hum+"&pres="+pres+" HTTP/1.1\r\nHost: "+host+"\r\nConnection: close\r\n\r\n");
     client.stop();                                      // Закрытие соединения
-    Serial.println("Client disconnected.");             // Печать "Клиент отключен"
-  }
 }
